3e91255050739adfc20407d11bf04a2c1f4a0394
djawny <djawny@op.pl>
2017-02-25 15:14:00 +0100

    zmiana

---
+++ app/src/main/java/com/sdaacademy/jawny/daniel/httpdropbox3/FileListAdapter.java
@@ -0,0 +1,38 @@
+package com.sdaacademy.jawny.daniel.httpdropbox3;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class FileListAdapter extends ArrayAdapter<DropboxFile> {
+
+    public FileListAdapter(Context context, ArrayList<DropboxFile> list) {
+        super(context, R.layout.list_row, list);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        DropboxFile file = getItem(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_row, parent, false);
+        }
+        TextView textView = (TextView) convertView.findViewById(R.id.text);
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.image);
+        if (file.getTag().equals("folder")) {
+            imageView.setVisibility(View.VISIBLE);
+
+        } else {
+            imageView.setVisibility(View.GONE);
+        }
+        textView.setText(file.getName());
+        return convertView;
+    }
+}
