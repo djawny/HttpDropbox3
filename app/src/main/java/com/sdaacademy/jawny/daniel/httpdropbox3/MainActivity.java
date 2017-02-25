f11497b2fc138d6509c51547fbcb8c79a06102f0
djawny <djawny@op.pl>
2017-02-25 09:58:05 +0100

    zmiana

--- app/src/main/java/com/sdaacademy/jawny/daniel/httpdropbox3/MainActivity.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/httpdropbox3/MainActivity.java
@@ -26,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.list)
     ListView mList;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
