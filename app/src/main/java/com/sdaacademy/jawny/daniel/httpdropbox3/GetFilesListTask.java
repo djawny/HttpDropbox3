b32fb43e05a71e6ef71629aa9eaf3ed215aa7c13
djawny <djawny@op.pl>
2017-02-25 11:09:32 +0100

    zmiana

---
+++ app/src/main/java/com/sdaacademy/jawny/daniel/httpdropbox3/GetFilesListTask.java
@@ -0,0 +1,81 @@
+package com.sdaacademy.jawny.daniel.httpdropbox3;
+
+import android.os.AsyncTask;
+import android.widget.ArrayAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class GetFilesListTask extends AsyncTask<String, Integer, JSONObject> {
+
+    private ArrayAdapter<DropboxFile> listAdapter;
+
+    public GetFilesListTask(ArrayAdapter<DropboxFile> listAdapter) {
+        this.listAdapter = listAdapter;
+    }
+
+    @Override
+    protected JSONObject doInBackground(String... params) {
+        try {
+            return sentRequest();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private List<DropboxFile> convert(JSONArray array) {
+        ArrayList<DropboxFile> list = new ArrayList<>();
+
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject jsonObject = array.optJSONObject(i);
+            DropboxFile dropboxFile = new DropboxFile();
+            dropboxFile.setName(jsonObject.optString("name"));
+            list.add(dropboxFile);
+        }
+        return list;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+
+    }
+
+    @Override
+    protected void onPostExecute(JSONObject jsonObject) {
+        super.onPostExecute(jsonObject);
+
+        JSONArray entries = jsonObject.optJSONArray("entries");
+        List<DropboxFile> dropboxFiles = convert(entries);
+        listAdapter.addAll(dropboxFiles);
+    }
+
+    private JSONObject sentRequest() throws IOException, JSONException {
+        MediaType jsonMediaType = MediaType.parse("application/json; charset=utf-8");
+        String json = new JSONObject().put("path", "").toString();
+        RequestBody body = RequestBody.create(jsonMediaType, json);
+        Request request = new Request.Builder()
+                .addHeader("Authorization", "Bearer 3TS3KjVdr6AAAAAAAAAAFJ1CyfWp14sQeIAppj6Mf9N4t37r3LgLDalWWXvzYbHm")
+                .addHeader("Content-Type", "application/json")
+                .url("https://api.dropboxapi.com/2/files/list_folder")
+                .post(body)
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        Response response = client.newCall(request).execute();
+        return new JSONObject(response.body().string());
+    }
+}
--- app/src/main/java/com/sdaacademy/jawny/daniel/httpdropbox3/MainActivity.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/httpdropbox3/MainActivity.java
@@ -1,27 +1,12 @@
 package com.sdaacademy.jawny.daniel.httpdropbox3;
 
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,64 +25,6 @@ public class MainActivity extends AppCompatActivity {
         listAdapter = new ArrayAdapter<DropboxFile>(this, android.R.layout.simple_dropdown_item_1line);
         mList.setAdapter(listAdapter);
 
-        new GetFilesListTask().execute();
-    }
-
-    public class GetFilesListTask extends AsyncTask<String, Integer, JSONObject> {
-
-        @Override
-        protected JSONObject doInBackground(String... params) {
-            try {
-                return sentRequest();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        private List<DropboxFile> convert(JSONArray array) {
-            ArrayList<DropboxFile> list = new ArrayList<>();
-
-            for (int i = 0; i < array.length(); i++) {
-                JSONObject jsonObject = array.optJSONObject(i);
-                DropboxFile dropboxFile = new DropboxFile();
-                dropboxFile.setName(jsonObject.optString("name"));
-                list.add(dropboxFile);
-            }
-            return list;
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            super.onProgressUpdate(values);
-
-        }
-
-        @Override
-        protected void onPostExecute(JSONObject jsonObject) {
-            super.onPostExecute(jsonObject);
-            Log.i(DROP_BOX, jsonObject.toString());
-
-            JSONArray entries = jsonObject.optJSONArray("entries");
-            List<DropboxFile> dropboxFiles = convert(entries);
-            listAdapter.addAll(dropboxFiles);
-        }
-
-        private JSONObject sentRequest() throws IOException, JSONException {
-            MediaType jsonMediaType = MediaType.parse("application/json; charset=utf-8");
-            String json = new JSONObject().put("path", "").toString();
-            RequestBody body = RequestBody.create(jsonMediaType, json);
-            Request request = new Request.Builder()
-                    .addHeader("Authorization", "Bearer 3TS3KjVdr6AAAAAAAAAAFJ1CyfWp14sQeIAppj6Mf9N4t37r3LgLDalWWXvzYbHm")
-                    .addHeader("Content-Type", "application/json")
-                    .url("https://api.dropboxapi.com/2/files/list_folder")
-                    .post(body)
-                    .build();
-            OkHttpClient client = new OkHttpClient();
-            Response response = client.newCall(request).execute();
-            return new JSONObject(response.body().string());
-        }
+        new GetFilesListTask(listAdapter).execute();
     }
 }
