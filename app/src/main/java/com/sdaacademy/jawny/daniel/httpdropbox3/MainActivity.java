56526be7bf0df7572160ff87ffcf096ef10ed196
djawny <djawny@op.pl>
2017-02-25 09:31:37 +0100

    zmiana

--- app/src/main/java/com/sdaacademy/jawny/daniel/httpdropbox3/MainActivity.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/httpdropbox3/MainActivity.java
@@ -1,15 +1,50 @@
 package com.sdaacademy.jawny.daniel.httpdropbox3;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import org.json.JSONObject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String DROP_BOX = "DropBox";
 
+    @BindView(R.id.list)
+    ListView mList;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
+
+    public class GetFilesListTask extends AsyncTask<String, Integer, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(String... params) {
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject jsonObject) {
+            super.onPostExecute(jsonObject);
+
+        }
+    }
+
+
 }
